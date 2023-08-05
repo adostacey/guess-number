71eaedeefd8c441ddc285b82916de166d6b4ff26
aaron-stacey <aaron.stacey@proton.me>
2023-08-05 15:10:48 -0500

    done for now

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
-// TODO: input validation
-// TODO: log/count entries
+// TODO: input validation, number in range -fixed
+// TODO: make displayed range inclusive - fixed
+// TODO: log/count entries - fixed
 // TODO: Play again?
 
 // html
 const btn = document.querySelector("#submit");
 const placeholder = document.querySelector("#placeholder");
+const guessLog = document.querySelector("#guessLog");
 let input = document.querySelector("#inputNumber");
 // variables
 let answer = Math.ceil(Math.random() * 1000);
-let range = [...Array(1000).keys()];
-
 console.log(answer);
+// let range = Array.from({ length: 1000 }, (v, k) => k + 1);
+let guesses = [];
+
+let lowEnd = 1;
+let highEnd = 1000;
 
 updateUI();
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  const guess = Number(input.value);
-
-  if (guess === answer) {
-    updateUI(true);
-  } else if (guess > answer) {
-    range = range.splice(0, range.indexOf(guess));
-    updateUI();
-  } else if (guess < answer) {
-    range = range.splice(range.indexOf(guess), range.length);
-    updateUI();
-  }
-
-  console.log(range);
+  checkGuess(Number(input.value));
+  console.log(guesses);
 });
 
 function updateUI(winState = false) {
-  let rangeText = "";
-  if (winState) {
-    rangeText = "WIN!";
-  } else {
-    rangeText = `${range[0]} to ${range[range.length - 1]}`;
-  }
+  let rangeText = winState
+    ? "GG! Play Again?"
+    : `Between ${lowEnd} and ${highEnd}`;
   placeholder.textContent = rangeText;
+
+  if (guesses.length > 0) {
+    let listItem = document.createElement("li");
+    listItem.innerText = input.value;
+    guessLog.appendChild(listItem);
+  }
   input.value = "";
+}
+
+function checkGuess(num) {
+  if (lowEnd > num || num > highEnd) {
+    alert("Number is outside of range!");
+    input.value = "";
+  } else if (num === answer) {
+    updateUI(true);
+  } else if (num > answer) {
+    highEnd = num - 1;
+    guesses.push(num);
+    updateUI();
+  } else if (num < answer) {
+    lowEnd = num + 1;
+    guesses.push(num);
+
+    updateUI();
+  }
 }
